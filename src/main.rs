db95959d53847bf2551bc67dd4caaba50d55c642
rPlakama <rPlakama@proton.me>
2025-11-14 19:33:03 -0300

    Initial calculations formule.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::io;
-fn main(){
-
+fn main() {
     let mut ncalc = String::new();
+    let mut resultado = 0;
 
-    io::stdin()
-        .read_line(&mut ncalc)
-        .expect("Failed");
+    io::stdin().read_line(&mut ncalc).expect("Failed");
 
     let ncalc_int: i32 = ncalc.trim().parse().expect("Not a valid number");
-    println!("{}", ncalc_int );
 
-
+    for i in 0..ncalc_int {
+        let i = i + 2;
+        resultado = i;
+    }
+    if resultado > 0 {
+        println!("Funciona.");
+    } else {
+        println!("Não funciona.");
+    }
 }
