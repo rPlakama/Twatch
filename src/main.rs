e0e0b3ffa850469a280bfa8df8c7afd23c4e9239
rPlakama <rPlakama@proton.me>
2025-11-14 19:51:10 -0300

    Defining current scope

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 use std::io;
 use std::time::Instant;
 fn main() {
-    let mut ncalc = String::new();
-    let mut resultado = 0;
+    let mut duration = String::new();
 
-    io::stdin().read_line(&mut ncalc).expect("Failed");
-
-    let ncalc_int: i32 = ncalc.trim().parse().expect("Not a valid number");
+    io::stdin().read_line(&mut duration).expect("Failed");
 
     let start = Instant::now();
-    for i in 0..ncalc_int {
-        let i = i + 2000;
-        resultado = i;
-    }
-    if resultado > 0 {
-        println!("Tempo Total: {:?}", start.elapsed());
-    };
+    println!("Tempo Total: {:?}", start.elapsed());
 }
