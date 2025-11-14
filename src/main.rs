ee5f9e4af7e9f1b9bc46598dfea16787ef599600
rPlakama <rPlakama@proton.me>
2025-11-14 20:28:08 -0300

    Definição de timer no main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
-use std::time::Instant;
+use std::time::Duration;
 fn main() {
-    let mut duration = String::new();
+    println!("Enter Duration(S):  ");
 
-    io::stdin().read_line(&mut duration).expect("Failed");
+    let mut input_duration = String::new();
+    io::stdin().read_line(&mut input_duration).expect("Failed");
+    let seconds: u64 = input_duration.trim().parse().expect(
+        "Enter a valid input, if it was a number, please use ( 0 to 18,446,744,073,709,551,615 )",
+    );
 
-    let start = Instant::now();
-    println!("Tempo Total: {:?}", start.elapsed());
+    let duration = Duration::from_secs(seconds);
+    while duration > Duration::from_secs(0) {
+        // Agora fazer a estrutura do loop 
+
+    }
 }
