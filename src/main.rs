3ea2e4e9677d4292c77cbcea6bd8a59097d75385
rPlakama <rPlakama@proton.me>
2025-11-17 09:00:30 -0300

    Countdown as function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,27 @@ use std::fs;
 use std::io::{self, Write};
 use std::time::{Duration, Instant};
 
+fn countdown() {
+    println!("Enter Duration(S):  ");
+    let mut _i = 0u64;
+
+    let mut input_duration = String::new();
+    io::stdin().read_line(&mut input_duration).expect("Failed");
+    let seconds: u64 = input_duration.trim().parse().expect(".");
+
+    let duration = Duration::from_secs(seconds);
+    let start = Instant::now();
+
+    while Instant::now() - start <= duration {
+        let elapse = Instant::now() - start;
+        let remaining = duration
+            .checked_sub(elapse)
+            .unwrap_or_else(|| Duration::from_secs(0));
+        print!("\rTime to end: {}s ", remaining.as_secs());
+        std::io::stdout().flush().unwrap();
+        while Instant::now() - start < duration {}
+    }
+}
 fn search() {
     let content = fs::read_to_string("/sys/class/hwmon/hwmon1/temp1_input")
         .expect("Should have been able to read the file.");
@@ -24,24 +45,6 @@ fn main() {
     if choice.trim() == "1" {
         search();
     } else if choice.trim() == "2" {
-        println!("Enter Duration(S):  ");
-        let mut _i = 0u64;
-
-        let mut input_duration = String::new();
-        io::stdin().read_line(&mut input_duration).expect("Failed");
-        let seconds: u64 = input_duration.trim().parse().expect(".");
-
-        let duration = Duration::from_secs(seconds);
-        let start = Instant::now();
-
-        while Instant::now() - start <= duration {
-            let elapse = Instant::now() - start;
-            let remaining = duration
-                .checked_sub(elapse)
-                .unwrap_or_else(|| Duration::from_secs(0));
-            print!("\rTime to end: {}s ", remaining.as_secs());
-            std::io::stdout().flush().unwrap();
-            while Instant::now() - start < duration {}
-        }
-    }
+        countdown();
+    };
 }
