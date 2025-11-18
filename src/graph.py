5176dc36fda30231da1bcdb7ddb9157ae684c50a
rPlakama <rPlakama@proton.me>
2025-11-18 19:12:22 -0300

    graph.py per last_session logic

--- src/graph.py
+++ src/graph.py
@@ -1,19 +1,21 @@
+import glob
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-df = pd.read_csv("../session_0.csv")
+sessions = glob.glob("../session_*")
+last_session = sessions[-1]
+df = pd.read_csv(f"{last_session}")
 
 
-plt.figure(figsize=(12,6))
-for (t, label), group in df.groupby(['Type', 'Label']):
-    plt.plot(group.index, group['Temp'], label=f"{t}-{label}")
+plt.figure(figsize=(12, 6))
+for (t, label), group in df.groupby(["Type", "Label"]):
+    plt.plot(group.index, group["Temp"], label=f"{t}-{label}")
 
-plt.xlabel('Sample Index')
-plt.ylabel('Temperature (°C)')
-plt.title('Temperature Trends by Sensor Type and Label')
-plt.legend(loc='upper left', fontsize='small')
+plt.xlabel("Duration")
+plt.ylabel("Temperature (°C)")
+plt.title("HWMON Devices Temperature")
+plt.legend(loc="upper left", fontsize="small")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-
