3d6c57b28f8717c52aa8591985b402c8fbb677f2
rPlakama <rPlakama@proton.me>
2025-11-25 18:28:46 -0300

    Nah

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        cargo-tauri
         rustfmt
         rust-analyzer
         cargo
