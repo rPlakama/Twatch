2a4f63e223c2a4ee3289d871ff68ca6c499fdbf7
rPlakama <rPlakama@proton.me>
2025-11-26 10:59:46 -0300

    I should have made a pr at this point

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::{
-    env,
     fs::{self, File},
     io::{self, Write},
     path::Path,
-    process::{self, Command},
+    process::Command,
     thread, time,
 };
 
@@ -17,6 +16,12 @@ pub struct SensorLabel {
     pub is_amd_gpu: bool,
     pub is_nvme: bool,
     pub temp: u32,
+}
+
+pub struct ArgumentPassers {
+    pub is_by_temperature: bool,
+    pub ms_delay: u64,
+    pub amount_captures: i16,
 }
 
 fn record_frame(
@@ -72,22 +77,46 @@ fn device_type(sensor: &SensorLabel) -> &'static str {
 }
 
 fn main() {
-    for argument in env::args() {
-        match argument.as_str() {
-            "-bt" | "--by-temperature" => {
-                trigger_by_temperature();
-            }
-            "-h" | "--help" => {
-                println!("-bt or --by-temperature \n Calls by-temperature function");
-                process::exit(0);
-            }
+    let args: Vec<String> = std::env::args().collect();
 
+    let mut args = std::env::args().skip(1);
+
+    let mut arg_passers = ArgumentPassers {
+        is_by_temperature: false,
+        ms_delay: 0,
+        amount_captures: 0,
+    };
+
+    let mut start_temp: u32 = 0;
+    let mut end_temp: u32 = 0;
+
+    while let Some(arg) = args.next() {
+        match &arg[..] {
+            "-bt" | "--by-temperature" => {
+                println!("Works");
+                arg_passers.is_by_temperature = true;
+            }
+            "-d" | "--delay" => {
+                if let Some(val_str) = args.next() {
+                    arg_passers.ms_delay = val_str.parse().unwrap_or(250);
+                    println!("Not only works, but here the ms: {}", arg_passers.ms_delay);
+                } else {
+                    eprintln!("Error: -d | --d requires (value) in (ms)");
+                    return;
+                }
+            }
+            "-c" | "--captures" => {
+                if let Some(val_str) = args.next() {
+                    arg_passers.amount_captures = val_str.parse().unwrap_or(10);
+                }
+            }
             _ => {
-                println!("Argument not found: {}", argument);
+                println!("Unknown argument: {}", arg);
             }
         }
     }
 }
+
 fn search_sensors() -> std::io::Result<Vec<SensorLabel>> {
     let mut collected_data: Vec<SensorLabel> = Vec::new();
 
