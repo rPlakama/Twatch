28da070b136695bb36776a02017bd9d63ca83b79
rPlakama <rPlakama@proton.me>
2026-06-11 20:18:15 -0300

    chore(dependecie): Implement rustc depedency onto nix flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-  {
+{
   description = "Twatch package";
 
   inputs = {
@@ -6,46 +6,52 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      formatter = pkgs.alejandra;
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.alejandra;
 
-      packages.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "twatch";
-        version = "0.1.5";
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "twatch";
+          version = "0.1.5";
 
-        src = ./.;
+          src = ./.;
 
-        cargoLock.lockFile = ./Cargo.lock;
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-        buildInputs = with pkgs; [
-          gtk4
-        ];
-      };
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            gtk4
+          ];
+        };
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          cargo
-          pkg-config
-        ];
-        buildInputs = with pkgs; [
-          rustfmt
-          rust-analyzer
-          gtk4
-          gh
-        ];
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            rustfmt
+            rust-analyzer
+            rustc
+            gtk4
+            gh
+          ];
 
-        shellHook = ''
-        rust-analyzer --version; gtk4 --version; gh --version; cargo --version; echo "Loaded!"; fish
-        '';
-      };
-    });
+          shellHook = ''
+            rust-analyzer --version; gtk4 --version; gh --version; cargo --version; echo "Loaded!"; fish
+          '';
+        };
+      }
+    );
 }
